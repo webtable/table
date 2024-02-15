b7c78c83f5d43a4db7e73e3e094b81a5469a2852
Sergio Alcantara <serg-io@users.noreply.github.com>
2024-02-15 08:07:50 -0800

    Updated dependencies.

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,7 +2,7 @@ import type { StorybookConfig } from '@storybook/web-components-vite';
 
 export default {
 	stories: ['../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-	addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+	addons: ['@storybook/addon-essentials'],
 	framework: {
 		name: '@storybook/web-components-vite',
 		options: {},
